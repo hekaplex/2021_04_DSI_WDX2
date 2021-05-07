314dbb041dced1be639358e24c97b5d9fa1aa6ff
ComplexDesigns <83430544+ComplexDesigns@users.noreply.github.com>
2021-05-07 15:51:20 -0400

    Delete Exercise 4.7.sql

--- Exercise 4.7.sql
+++
@@ -1,4 +0,0 @@
-Select distinct V1.VendorName, V1.VendorCity, V1.VendorState 
-from Vendors V1 
-WHERE V1.VendorCity NOT IN (Select VendorCity from Vendors V2 where V2.VendorID <> V1.VendorID) 
-and V1.VendorState NOT IN (Select VendorState from Vendors V3 where V3.VendorID <> V1.VendorID); 
