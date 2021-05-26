6e798a38a9e0f42098e0260c0b9e4af50770ac80
JAMROG71 <51856898+JAMROG71@users.noreply.github.com>
2021-05-26 14:34:28 -0400

    Python Math Multiply

---
+++ Rogers James/Python Adding.py
@@ -0,0 +1,7 @@
+num1 = 91.38
+num2 = 65.28
+num3 = 75.27
+
+sum = num1 + num2 + num3
+
+print('The sum of {0}, {1}, and {2} = {3}'.format(num1, num2, num3, sum))
--- Rogers James/test.py
+++
@@ -1,5 +0,0 @@
-first_name=input()
-generic_location = input()
-whole_number = input()
-plural_noun = input()
-print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
