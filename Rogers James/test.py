511d3228288a0ff9b4e32a0cae422dcaa26ddd6c
JAMROG71 <51856898+JAMROG71@users.noreply.github.com>
2021-05-26 14:13:05 -0400

    Python Math Adding

---
+++ Rogers James/test.py
@@ -0,0 +1,5 @@
+first_name=input()
+generic_location = input()
+whole_number = input()
+plural_noun = input()
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
